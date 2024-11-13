22d59ecb4c71b7308ddb7e2bbe7928ec8ffce1fd
Rubin Imeri <rubinimeri6@gmail.com>
2024-11-13 23:57:07 +0100

    Add PostCard component & utility classes

--- frontend/public-client/src/components/Hero.jsx
+++ frontend/public-client/src/components/Hero.jsx
@@ -1,3 +1,6 @@
+import posts from "@/utils/exampleData.js";
+import PostCard from "@/components/PostCard.jsx";
+
 function Hero() {
     return (
         <main className="mx-[70px] max-sm:mx-0 text-center">
@@ -10,24 +13,25 @@ function Hero() {
             <section className="">
                 <img src="/hero-img-placeholder.jpg" alt="hero image"/>
                 <div className="p-6 flex flex-col max-sm:gap-3 tracking-wider">
-                    <p className="text-gray-500 text-sm font-bold">rubinimeri - 8 minutes ago</p>
+                    <p className="">rubinimeri - 8 minutes ago</p>
                     <h1 className="font-serif font-black max-sm:text-[34px]">
                         Walking as a Form of
                         Meditation
                     </h1>
-                    <p className="text-gray-500 text-xs font-bold tracking-tight text-left">
+                    <p className="content-hero text-left">
                         Reflect on how a walk in nature can become a mindful experience, sharpening the senses and grounding us in the present. Highlight the rustling of leaves, the feel of a breeze, and the chorus of bird songs as simple yet profound experiences that elevate walking..
                     </p>
                     <p>
-                        <a className="text-blue-500 text-[15px] font-bold underline" href="#">Read more</a>
+                        <a className="link" href="#">Read more</a>
                     </p>
                 </div>
             </section>
             <section className="p-6">
                 <div className="flex justify-between">
                     <h2 className="font-bold max-sm:text-2xl">Latest Posts</h2>
-                    <a className="text-blue-500 text-[15px] font-bold underline" href="#">See All</a>
+                    <a className="link" href="#">See All</a>
                 </div>
+                {posts.map(post => <PostCard key={post.id} {...post} />)}
             </section>
         </main>
     );
