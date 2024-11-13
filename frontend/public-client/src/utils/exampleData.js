548ea01e665c2ec89fab125faee0c28a32a56282
Rubin Imeri <rubinimeri6@gmail.com>
2024-11-13 23:37:31 +0100

    Add example data for posts

---
+++ frontend/public-client/src/utils/exampleData.js
@@ -0,0 +1,36 @@
+const posts = [
+    {
+        id: 2,
+        author: "John Smith",
+        title: "10 Tips for Productive Remote Work",
+        content: "Learn essential tips to stay productive while working remotely.",
+        imageUrl: "https://plus.unsplash.com/premium_photo-1721605863749-72a6e2433d71?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        dateAdded: "2024-10-05",
+    },
+    {
+        id: 3,
+        author: "Alice Johnson",
+        title: "Healthy Eating on a Budget",
+        content: "Discover strategies to eat healthy without breaking the bank.",
+        imageUrl: "https://plus.unsplash.com/premium_photo-1663852297555-e2c68137106b?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        dateAdded: "2024-10-10",
+    },
+    {
+        id: 4,
+        author: "Robert Lee",
+        title: "Travel Guide: Top Destinations for 2024",
+        content: "A curated list of must-visit destinations around the world in 2024.",
+        imageUrl: "https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        dateAdded: "2024-10-15",
+    },
+    {
+        id: 5,
+        author: "Emma Brown",
+        title: "The Benefits of Daily Meditation",
+        content: "Exploring the mental and physical benefits of incorporating meditation into your daily routine.",
+        imageUrl: "https://images.unsplash.com/photo-1499728603263-13726abce5fd?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        dateAdded: "2024-10-20",
+    },
+];
+
+export default posts;
