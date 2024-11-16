30a1d604639f9ad99caeb2bf94260a22013d0542
Rubin Imeri <rubinimeri6@gmail.com>
2024-11-16 14:31:15 +0100

    Use cors middleware to allow the api to be used

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes/index.js';
 import globalErrorHandler from './controllers/errorController.js'
 import CustomError from "./utils/customError.js";
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
