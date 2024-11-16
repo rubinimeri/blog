4f23ac8a0f128a093b4d6526593668b9bd0f7359
Rubin Imeri <rubinimeri6@gmail.com>
2024-11-16 23:48:12 +0100

    Add usePost hook

---
+++ frontend/public-client/src/hooks/usePost.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+const usePost = (postId) => {
+  const [post, setPost] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_BASE_URL}/posts/${postId}`, {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((data) => setPost(data))
+      .catch((error) => setError(error))
+      .finally(setLoading(false));
+  }, [postId]);
+
+  return { loading, error, post };
+};
+
+export default usePost;
