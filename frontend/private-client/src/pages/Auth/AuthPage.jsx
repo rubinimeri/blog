77fcf5d8703dceacd4872611a911cebe3f1129b8
Rubin Imeri <rubinimeri6@gmail.com>
2024-11-19 22:19:18 +0100

    Add dark/light logos

--- frontend/private-client/src/pages/Auth/AuthPage.jsx
+++ frontend/private-client/src/pages/Auth/AuthPage.jsx
@@ -10,21 +10,37 @@ import { ModeToggle } from "@/components/mode-toggle.jsx";
 
 function AuthPage() {
   return (
-    <main className="h-screen flex justify-center items-center">
-      <Tabs defaultValue="signUp" className="w-[400px]">
+    <>
+      <header className="flex items-center justify-center gap-2 p-4">
+        <img
+          src="/logo-dark.png"
+          alt="logo"
+          className="hidden dark:block"
+          width={300}
+        />
+        <img
+          src="/logo-light.png"
+          alt="logo"
+          className="block dark:hidden"
+          width={300}
+        />
         <ModeToggle />
-        <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value="login">Login</TabsTrigger>
-          <TabsTrigger value="signUp">Sign Up</TabsTrigger>
-        </TabsList>
-        <TabsContent value="login">
-          <Login />
-        </TabsContent>
-        <TabsContent value="signUp">
-          <SignUp />
-        </TabsContent>
-      </Tabs>
-    </main>
+      </header>
+      <main className="min-h-[85vh] flex justify-center items-center">
+        <Tabs defaultValue="signUp" className="w-[400px]">
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="login">Login</TabsTrigger>
+            <TabsTrigger value="signUp">Sign Up</TabsTrigger>
+          </TabsList>
+          <TabsContent value="login">
+            <Login />
+          </TabsContent>
+          <TabsContent value="signUp">
+            <SignUp />
+          </TabsContent>
+        </Tabs>
+      </main>
+    </>
   );
 }
 
