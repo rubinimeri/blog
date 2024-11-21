62bb0bba958d9cdc6a023d63f742a11394dd8da1
Rubin Imeri <rubinimeri6@gmail.com>
2024-11-21 17:34:19 +0100

    Increase payload limit to 5mb

--- backend/app.js
+++ backend/app.js
@@ -6,8 +6,8 @@ import CustomError from "./utils/customError.js";
 const app = express();
 
 app.use(cors())
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ limit: '5mb', extended: true }));
 
 app.use('/posts', routes.messages);
 app.use('/posts', routes.posts);
